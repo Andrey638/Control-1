82f5fb920b27d7557d5991a876cda574c896e031
Andrey3544 <box4435@mail.ru>
2022-12-26 20:43:49 +0300

    task1

--- task/Program.cs
+++ task/Program.cs
@@ -2,7 +2,7 @@
 // длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array1 = new string[5] {"453", "67", "hello", "string", "new"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
